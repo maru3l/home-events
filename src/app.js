d66d59fb4b1749fd132ecd9fa83f3836c7670dee
Marc-Antoine Ruel <hello@marcantoineruel.com>
2022-01-16 12:23:49 -0500

    fix endpoint for camera query params

--- src/app.js
+++ src/app.js
@@ -14,9 +14,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.get('/driveway/motion/on', (req, res) => {
+app.get('/driveway/motion', (req, res) => {
   try {
-    publish('home/driveway/camera/motion', 'ON');
+    if (req.query.message === 'ON') {
+      publish('home/driveway/camera/motion', 'ON');
+    }
+
+    if (req.query.message === 'OFF') {
+      publish('home/driveway/camera/motion', 'OFF');
+    }
 
     res.json({ response: 'OK' });
   } catch (error) {
@@ -28,33 +34,15 @@ app.get('/driveway/motion/on', (req, res) => {
   }
 });
 
-app.get('/driveway/motion/off', (req, res) => {
-  try {
-    publish('home/driveway/camera/motion', 'OFF');
-
-    res.json({ response: 'OK' });
-  } catch (error) {
-    logger.error(error);
-
-    res.status(500).json({ error });
-  }
-});
-
 app.get('/front_yard/motion/on', (req, res) => {
   try {
-    publish('home/front_yard/camera/motion', 'ON');
+    if (req.query.message === 'ON') {
+      publish('home/front_yard/camera/motion', 'ON');
+    }
 
-    res.json({ response: 'OK' });
-  } catch (error) {
-    logger.error(error);
-
-    res.status(500).json({ error });
-  }
-});
-
-app.get('/front_yard/motion/off', (req, res) => {
-  try {
-    publish('home/front_yard/camera/motion', 'OFF');
+    if (req.query.message === 'OFF') {
+      publish('home/front_yard/camera/motion', 'OFF');
+    }
 
     res.json({ response: 'OK' });
   } catch (error) {
