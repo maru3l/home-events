bf3f56763f7a6b0f78a15334a8ca8e571d40b374
Marc-Antoine Ruel <hello@marcantoineruel.com>
2022-01-16 12:23:22 -0500

    remove log file

--- src/logger.js
+++ src/logger.js
@@ -13,8 +13,8 @@ const logger = createLogger({
         format.errors({ stack: true }),
       ),
     }),
-    new transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new transports.File({ filename: 'logs/combined.log' }),
+    // new transports.File({ filename: 'logs/error.log', level: 'error' }),
+    // new transports.File({ filename: 'logs/combined.log' }),
   ],
 });
 
